75a4f15d569c0d3d371c7a4b72b28dbecbee5192
Mals12 <zS170133@estudiantes.uv.mx>
2021-05-26 19:20:54 -0500

    Se creo el programa para el metodo de busqueda A*

---
+++ Bus_A.py
@@ -0,0 +1,23 @@
+from Diccionarios.distancias import distancias_lineales
+from Diccionarios.Vecinos import vecinos
+from Diccionarios.Costo import costo
+
+def A(inicio, destino):
+    dis_lin = distancias_lineales[destino]
+    arbol = [[dis_lin[inicio], inicio]]
+    recorrido = []
+    while len(arbol) != 0:
+        arbol_ord = sorted(arbol, key=lambda arbol: arbol[0])
+        primero = arbol_ord[0]
+        ciudad_prim = primero[1]
+        cos = costo[ciudad_prim]
+        recorrido.append(ciudad_prim)
+        arbol.pop(0)
+        if ciudad_prim == destino:
+            re_final = recorrido
+            break
+        else:
+            for i in vecinos[ciudad_prim]:
+                arb = [dis_lin[i]+cos[i], i]
+                arbol.append(arb)
+    return re_final
