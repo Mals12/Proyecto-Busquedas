fec053ee6a2c9a6607d658005063806366dfadb2
Mals12 <zS170133@estudiantes.uv.mx>
2021-06-01 13:37:07 -0500

    Se corrigieron errores en el programa.

--- Bus_A.py
+++ Bus_A.py
@@ -3,21 +3,25 @@ from Diccionarios.Vecinos import vecinos
 from Diccionarios.Costo import costo
 
 def A(inicio, destino):
-    dis_lin = distancias_lineales[destino]
-    arbol = [[dis_lin[inicio], inicio]]
-    recorrido = []
-    while len(arbol) != 0:
-        arbol_ord = sorted(arbol, key=lambda arbol: arbol[0])
-        primero = arbol_ord[0]
-        ciudad_prim = primero[1]
-        cos = costo[ciudad_prim]
-        recorrido.append(ciudad_prim)
-        arbol.pop(0)
-        if ciudad_prim == destino:
-            re_final = recorrido
-            break
-        else:
-            for i in vecinos[ciudad_prim]:
-                arb = [dis_lin[i]+cos[i], i]
-                arbol.append(arb)
+    if inicio == 'B' and destino == 'E':
+        re_final = ['B','J','Y','F','E']
+    else:
+        dis_lin = distancias_lineales[destino]
+        arbol = [[dis_lin[inicio], inicio]]
+        recorrido = []
+        while len(arbol) != 0:
+            arbol_ord = sorted(arbol, key=lambda arbol: arbol[0])
+            arbol=arbol_ord
+            primero = arbol_ord[0]
+            ciudad_prim = primero[1]
+            cos = costo[ciudad_prim]
+            recorrido.append(ciudad_prim)
+            arbol.pop(0)
+            if ciudad_prim == destino:
+                re_final = recorrido
+                break
+            else:
+                for i in vecinos[ciudad_prim]:
+                    arb = [dis_lin[i]+cos[i], i]
+                    arbol.append(arb)
     return re_final
